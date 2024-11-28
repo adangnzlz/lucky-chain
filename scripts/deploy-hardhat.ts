6a18d266c56e19b6f921728ebd2bf5ae02c0bfc9
Adán González <watson@adntro.com>
2024-11-28 16:24:14 +0000

    update deploy hardhat localhost config and remove unnusued files

--- scripts/deploy-hardhat.ts
+++ scripts/deploy-hardhat.ts
@@ -2,20 +2,32 @@ import { ethers } from "hardhat";
 
 async function main() {
   // Desplegar el mock VRFCoordinator en la red local
-  const VRFCoordinatorV2Mock = await ethers.getContractFactory("VRFCoordinatorV2Mock");
+  const VRFCoordinatorV2Mock = await ethers.getContractFactory(
+    "VRFCoordinatorV2Mock"
+  );
   const vrfCoordinatorMock = await VRFCoordinatorV2Mock.deploy(0, 0); // Inicialización simple
   await vrfCoordinatorMock.deployed();
-  console.log("Mock VRFCoordinator deployed to:", vrfCoordinatorMock.address);
+  console.log("Mock VRFCoordinator deployed to local network at:", vrfCoordinatorMock.address);
 
   // Desplegar la lotería con el mock
   const Lottery = await ethers.getContractFactory("Lottery");
   const subscriptionId = 1; // Cualquier valor para simular
   const keyHash = ethers.constants.HashZero; // Hash dummy
 
-  const lottery = await Lottery.deploy(subscriptionId, vrfCoordinatorMock.address, keyHash);
+  const lottery = await Lottery.deploy(
+    subscriptionId,
+    vrfCoordinatorMock.address,
+    keyHash
+  );
   await lottery.deployed();
 
   console.log("Lottery deployed to local network at:", lottery.address);
+
+  const LinkToken = await ethers.getContractFactory("LinkTokenMock");
+  const linkToken = await LinkToken.deploy();
+  await linkToken.deployed();
+  console.log("LinkToken deployed to local network at:", linkToken.address);
+
 }
 
 main().catch((error) => {
